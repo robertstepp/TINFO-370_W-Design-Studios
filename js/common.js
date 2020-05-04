d68a73ee1e72aac4c68973220772d9688327589c
Robert Stepp, robert@robertstepp.ninja <Robert Stepp, robert@robertstepp.ninja>
2020-05-03 19:04:36 -0700

    Added index and modified common.js

---
+++ js/common.js
@@ -0,0 +1,30 @@
+window.onload = function navigation() {
+    this.document.getElementById("navigation").innerHTML =
+        '<li id="home"><a href="home.html">Home</a></li>' +
+        '<li id="contactUs"><a href="contactus.html">Contact Us</a></li>' +
+        '<li id="aboutUs"><a href="aboutus.html">About Us</a></li>';
+        this.active();
+        this.footer();
+}
+
+function active() {
+    /* Gets page name without the extension */
+    var theLocation = location.href.split("/").slice(-1)[0].split(".")[0]; // https://stackoverflow.com/questions/16611497/how-can-i-get-the-name-of-an-html-page-in-javascript
+    switch (theLocation) {
+        case "index":
+            document.getElementById("home").setAttribute("id", "active");
+            break;
+        case "contactus":
+            document.getElementById("contactUs").setAttribute("id", "active");
+            break;
+        case "aboutus":
+            document.getElementById("aboutUs").setAttribute("id", "active");
+            break;
+    }
+}
+
+function footer() {
+    this.document.getElementById("footer").innerHTML = '<hr>' +
+    '<img src="images/logo.png" alt="W Design Studios" style="width:200px">' +
+    '&copy; 2020';
+}
